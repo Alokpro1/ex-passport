812f628f81863730831c6e9854f809cbf603b504
Alok <jyotirmayamahanta16@gmail.com>
2019-06-04 21:01:21 +0530

    Req.status issue fixed

--- app.js
+++ app.js
@@ -1,84 +1,64 @@
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
-const session = require('express-session') // for saving cookies : more session id
-const cors = require('cors')              // for having or sending req across diff domains
-const mongoose = require('mongoose')
-const errorHandeler = require('errorhandler')
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const errorHandler = require('errorhandler');
 
-//MOdules & routes
-require('./models/User');
-require('./config/passport');
-//app.use(require('./routes'));
-// to global variable
-
+//Configure mongoose's promise to global promise
 mongoose.promise = global.Promise;
 
-
-// 
+//Configure isProduction variable
 const isProduction = process.env.NODE_ENV === 'production';
-
 
 //Initiate our app
 const app = express();
 
-//config
+//Configure our app
 app.use(cors());
 app.use(require('morgan')('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
-app.use(express.static(path.join(__dirname,'public')));
-app.use(session({
-    secret: 'passport', 
-    cookie:{
-        maxAge: 60000
-    }, 
-    resave: false, 
-    saveUninitialized: false
-}));
+if(!isProduction) {
+  app.use(errorHandler());
+}
 
-//MOdules & routes
-// require('./models/User');
-// require('./config/passport');
+//Configure Mongoose
+mongoose.connect('mongodb://localhost/passport-tutorial');
+mongoose.set('debug', true);
+
+//Models & routes
+require('./models/User');
+require('./config/passport');
 app.use(require('./routes'));
-if(!isProduction){
-    app.use(errorHandeler());
-}
 
-const MongoClient = require('mongodb').MongoClient
-MongoClient.connect("mongodb://localhost:27017/YourDB", { useNewUrlParser: true },()=>{
-    console.log('mongodb connect');
-})
+//Error handlers & middlewares
+if(!isProduction) {
+  app.use((err, req, res,next) => {
+    res.status( 500);
 
-// //MOdules & routes
-// require('./models/User');
-// require('./config/passport');
-// app.use(require('./routes'));
-
-
-if(!isProduction){
-    app.use(function errorHandeler (err,req,res){
-        res.status(err.status || 500);
-
-        res.json({
-            erros:{
-                message:err.message,
-                error: err
-            },
-        });
-    });
-}
-
-app.use((err, req, res,next) => {
-    res.status(err.status || 500);
-  
     res.json({
       errors: {
         message: err.message,
-        error: {},
+        error: err,
       },
     });
   });
+}
 
-  app.listen(8000, function(){ console.log('Server running on http://localhost:8000/')});
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
+
+app.listen(8000, () => console.log('Server running on http://localhost:8000/'));
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,91 +4,82 @@ const router = require('express').Router();
 const auth = require('../auth');
 const Users = mongoose.model('Users');
 
+//POST new user route (optional, everyone has access)
+router.post('/', auth.optional, (req, res, next) => {
+  const { body: { user } } = req;
 
+  if(!user.email) {
+    return res.status(422).json({
+      errors: {
+        email: 'is required',
+      },
+    });
+  }
 
-//POST new user route 
-router.post('/',auth.optional,(req,res,next)=>{
-    const {body: {user}} =req;
-    // console.log(req.body);
-    // console.log(user);
+  if(!user.password) {
+    return res.status(422).json({
+      errors: {
+        password: 'is required',
+      },
+    });
+  }
 
-    if(!user.email){
-        return res.status(422).json({
-            errors:{
-                email: 'is required',
-            },
-        });
-    }
+  const finalUser = new Users(user);
 
-    if(!user.password) {
-        return res.status(422).json({
-          errors: {
-            password: 'is required',
-          },
-        });
-    }
+  finalUser.setPassword(user.password);
 
-    const finalUser = new Users(user);
-    
-    console.log(finalUser.email);
-     finalUser.setPassword(user.password);
-     var float =finalUser.toAuthJSON();
-     console.log("float:",float)
-     finalUser.save()
-    //console.log(finalUser.token);
-    res.send( float);
-    
+  return finalUser.save()
+    .then(() => res.json({ user: finalUser.toAuthJSON() }));
 });
 
-//POST login route
-
+//POST login route (optional, everyone has access)
 router.post('/login', auth.optional, (req, res, next) => {
-    const { body: { user } } = req;
-  
-    if(!user.email) {
-      return res.status(422).json({
-        errors: {
-          email: 'is required',
-        },
-      });
-    }
-  
-    if(!user.password) {
-      return res.status(422).json({
-        errors: {
-          password: 'is required',
-        },
-      });
-    }
-  
-    return passport.authenticate('local', { session: false }, (err, passportUser, info) => {
-      if(err) {
-        return next(err);
-      }
-  
-      if(passportUser) {
-        const user = passportUser;
-        user.token = passportUser.generateJWT();
-  
-        return res.json({ user: user.toAuthJSON() });
-      }
-  
-      return status(400).info;
-    })(req, res, next);
-  });
+  const { body: { user } } = req;
 
-  //GET current route (required, only authenticated users have access)
+  if(!user.email) {
+    return res.status(422).json({
+      errors: {
+        email: 'is required',
+      },
+    });
+  }
+
+  if(!user.password) {
+    return res.status(422).json({
+      errors: {
+        password: 'is required',
+      },
+    });
+  }
+
+  return passport.authenticate('local', { session: false }, (err, passportUser, info) => {
+    if(err) {
+      return next(err);
+    }
+
+    if(passportUser) {
+      const user = passportUser;
+      user.token = passportUser.generateJWT();
+
+      return res.json({ user: user.toAuthJSON() });
+    }
+
+    return status(400).info;
+  })(req, res, next);
+});
+
+//GET current route (required, only authenticated users have access)
 router.get('/current', auth.required, (req, res, next) => {
-    const { payload: { id } } = req;
-  
-    return Users.findById(id)
-      .then((user) => {
-        if(!user) {
-          return res.sendStatus(400);
-        }
-  
-        return res.json({ user: user.toAuthJSON() });
-      });
-  });
-  
-  module.exports = router;
+  const { payload: { id } } = req;
+
+  return Users.findById(id)
+    .then((user) => {
+      if(!user) {
+        return res.sendStatus(400);
+      }
+
+      return res.json({ user: user.toAuthJSON() });
+    });
+});
+
+module.exports = router;
